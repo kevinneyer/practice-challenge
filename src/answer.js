0feef7999421152d3af7543d16cdf9b75552a719
kevinneyer <kevinneyer@gmail.com>
2020-12-04 16:51:28 -0500

    practice changing time color if seconds are odd"

--- src/answer.js
+++ src/answer.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 
 const Answer = (props) => {
-  const [time, setTime] = useState(null);
+  // const [time, setTime] = useState(null);
   const [date, setDate] = useState(null);
+  const [seconds, setSeconds] = useState(null)
+  const [hours, setHours] = useState(null)
+  const [minutes, setMinutes] = useState(null)
 
   useEffect(() => {
     const timer = setInterval(() => dateAndTime(), 1000);
@@ -13,7 +16,10 @@ const Answer = (props) => {
 
   const dateAndTime = () => {
     setDate(new Date().toLocaleDateString())
-    setTime(new Date().toLocaleTimeString())
+    // setTime(new Date().toLocaleTimeString())
+    setHours(new Date().getHours())
+    setMinutes(new Date().getMinutes())
+    setSeconds(new Date().getSeconds())
   }
 
   const { input } = props
@@ -35,7 +41,11 @@ const Answer = (props) => {
       {input === undefined || input === false ? (
         <div>
           <h3>Today's Date: {date}</h3>
-          <h3>Current Time: {time}</h3>
+          {seconds % 2 === 0 ?  
+            <h3>Current Time: <div>{hours}:{minutes}:{seconds}</div></h3> 
+          : 
+            <h3>Current Time: <div className='time'>{hours}:{minutes}:{seconds}</div></h3> 
+          }
         </div>
       ) : 
       null
